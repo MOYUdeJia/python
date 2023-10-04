1241579e61c48f3d07b22ffc1f0b990994da633a
凉冰 <126070813+MOYUdeJia@users.noreply.github.com>
2023-10-04 22:09:56 +0800

    Add files via upload

---
+++ Python-学习之旅/main.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.pensize(5)
+turtle.color('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+turtle.mainloop()
+
+
